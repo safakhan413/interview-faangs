2da755a71aa3df0603e8ec2d8b29b7ee58d200a4
safakhan413 <49034994+safakhan413@users.noreply.github.com>
2020-12-21 08:49:18 +0800

    compressed string

---
+++ cracking-the-coding-interview/Chap-01/str_compression.py
@@ -0,0 +1,26 @@
+import re
+from collections import Counter
+
+#Counter(aabcccccaaa) gives a dict 
+
+def compress(string):
+  if len(string) == 0:
+    return string
+  parts = []
+  current_letter = string[0]
+  current_count = 1
+  for letter in string[1:]:
+    if current_letter == letter:
+      current_count += 1
+    else:
+      parts.append(current_letter + str(current_count))
+      current_letter = letter
+      current_count = 1
+  parts.append(current_letter + str(current_count))
+  compressed = "".join(parts)
+  if len(compressed) < len(string):
+    return compressed
+  else:
+    return string
+
+print(compress('aabcccccaaa'))
