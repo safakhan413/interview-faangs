777944d17a1c03f3e7354c3cbb475c0d286fb9ed
safakhan413 <49034994+safakhan413@users.noreply.github.com>
2020-12-27 23:58:39 +0500

    stack with max

---
+++ ELPI/Stacks and Queues/stack-with-max.py
@@ -0,0 +1,29 @@
+from collections import namedtuple
+
+class Stack:
+    EWCM = namedtuple('EWCM', ('x', 'max'))# EWCM is element with cached max
+
+    def __init__(self):
+        self._s = []
+
+    def empty(self):
+        return len(self._s) == 0
+
+    def max(self):
+        if self.empty():
+            raise IndexError('max(): empty stack')
+        return self._s[-1].max
+
+    def pop(self):
+        if self.empty():
+            raise IndexError('pop(): empty stack')
+        return self._s.pop().element
+
+    def push(self, x):
+        self._s.append(
+            self.EWCM(x, x if self.empty() else max(x, self.max())))
+
+st = Stack()
+st.push(5)
+st.push(8)
+print(st.max())
