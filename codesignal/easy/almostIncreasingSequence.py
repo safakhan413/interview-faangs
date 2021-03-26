7d22b0cffafc199d93c0668e852784494eeff8a7
safakhan413 <49034994+safakhan413@users.noreply.github.com>
2021-03-26 14:33:20 -0400

    almot increasing

--- codesignal/easy/almostIncreasingSequence.py
+++ codesignal/easy/almostIncreasingSequence.py
@@ -1,18 +1,27 @@
-def almostIncreasingSequence(sequence):
-    print(sequence)
-    # observation: if two times the value goes down or anytime more than 1 there cant be only 1 element after whose removal it becomes
-    # l = list()
-    s = 0
-    for i in range(0, len(sequence) - 1):
-        if sequence[i + 1] - sequence[i] <= 0: s += 1
-    if s > 1:
-        return False
-    else:
-        return True
-    #     l.append(sequence[i+1]- sequence[i])
-    # for i in l:
+def first_bad_pair(sequence):
+    """Return the first index of a pair of elements where the earlier
+    element is not less than the later elements. If no such pair
+    exists, return -1."""
+    for i in range(len(sequence)-1):
+        if sequence[i] >= sequence[i+1]:
+            return i
+    return -1
 
-    print(l)
+def almostIncreasingSequence(sequence):
+    """Return whether it is possible to obtain a strictly increasing
+    sequence by removing no more than one element from the array."""
+    j = first_bad_pair(sequence)
+    if j == -1:
+        return True  # List is increasing
+    if first_bad_pair(sequence[j-1:j] + sequence[j+1:]) == -1:
+        print(f'im value of j{sequence[j-1:j]+ sequence[j+1:]}')
+        return True  # Deleting earlier element makes increasing
+    if first_bad_pair(sequence[j:j+1] + sequence[j+2:]) == -1:
+        print(f'im value of i{j}')
+        return True  # Deleting later element makes increasing
+    return False  # Deleting either does not make increasingence[i+1]- sequence[i])
+sequence = [1, 3, 2]
+print(almostIncreasingSequence(sequence))
 
 
 
